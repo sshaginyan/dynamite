c5fb5927ab7ba408b986a25b1637bf6cc481627a
Steven Shaginyan <steven.shaginyan@heroku.com>
2018-12-06 03:16:09 -0800

    Testing

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+const number = 1;
+
+describe('1 & ' + number, function() {
+    it('should equal each other', function() {
+        assert.equal(1, number);
+    });
+});
